639f54235b1e250589a42329f7cee1a49dc2d605
Tiplok <tiplok1@gmail.com>
2017-01-23 17:39:57 +0100

    Lobby code for private lobby and join these lobby with url/code

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -50,9 +50,11 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
+        // Creer une session invité à l'utilisateur s'il n'est pas connecté, récupérer ses infos sinon
         $player_host = new Player('test', "room_waiting");
         $em->persist($player_host);
         $em->flush();
+
 
         if($request->getMethod() == 'POST'){
             $data = $request->request->all();
@@ -81,6 +83,7 @@ class DefaultController extends Controller
                 $repository = $this->getDoctrine()->getRepository('AppBundle:Room');
                 $room = $repository->find($data['lobby_id']);
                 $new_player = new Player('toto', 'room_waiting');
+                var_dump($room->generateRoomCode());
 
                 $em->persist($new_player);
                 $em->flush();
@@ -92,6 +95,32 @@ class DefaultController extends Controller
 
                 // actually executes the queries (i.e. the INSERT query)
                 $em->flush();
+            }
+        }
+
+        // GET dans le cas où on veut rentrer dans un salon privé avec le code
+        if($request->getMethod() == 'GET'){
+            $data = $request->query->all();
+
+            if(isset($data['lobby_code'])){
+                $repository = $this->getDoctrine()->getRepository('AppBundle:Room');
+                $room = $repository->findOneBy(array("type" => $data['lobby_code']));
+
+                // On a trouvé le salon dans lequel il veut rentrer
+                if($room != null){
+
+                    $room->addParticipant($player_host);
+
+                    // tells Doctrine you want to (eventually) save the Room (no queries yet)
+                    $em->persist($room);
+
+                    // actually executes the queries (i.e. the INSERT query)
+                    $em->flush();
+
+                // Code d'un salon qui n'existe pas ou plus
+                } else {
+                    return $this->render('default/lobby.html.twig', ["error" => ["message" => "Salon introuvable"]]);
+                }
             }
         }
 
