f663546bb623b30487dbd19a5a7cc2efe11d528a
Tiplok <tiplok1@gmail.com>
2017-01-25 15:54:40 +0100

    fixed untracked file

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,13 +1,13 @@
 <?php
-    $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $db = parse_url('mysql://b52d1a652c093a:558dc316@eu-cdbr-west-01.cleardb.com/heroku_c00a0f39ccee3b4?reconnect=true');
 
     $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', $db['host']);
-    $container->setParameter('database_port', $db['port']);
-    $container->setParameter('database_name', substr($db["path"], 1));
-    $container->setParameter('database_user', $db['user']);
-    $container->setParameter('database_password', $db['pass']);
-    $container->setParameter('secret', getenv('SECRET'));
+    $container->setParameter('database_host', 'eu-cdbr-west-01.cleardb.com');
+    $container->setParameter('database_port', 3306);
+    $container->setParameter('database_name', 'heroku_c00a0f39ccee3b4');
+    $container->setParameter('database_user', 'b52d1a652c093a');
+    $container->setParameter('database_password', '558dc316');
+    $container->setParameter('secret', 'your_super_token');
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', null);
     $container->setParameter('mailer_host', null);
