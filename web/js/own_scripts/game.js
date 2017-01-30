1530a638d252e18f0a64980ee2367632bcdbbfb6
Tiplok <tiplok1@gmail.com>
2017-01-30 19:12:55 +0100

    Score for each participant with split animation

--- web/js/own_scripts/game.js
+++ web/js/own_scripts/game.js
@@ -39,24 +39,22 @@ function validate(form_id) {
     function frame() {
 
         // Quand le score est positionné à 50%, on arrête et on change de page
-        if (pos == 50) {
+        if (pos == 46) {
+
+            /* Il faut faire grossir le pseudo de celui qui obtient le meilleur score
             if(form_id == 'proposition1_form') {
                 $("#player1_pseudo").css("font-size", "20px");
             } else {
                 $("#player2_pseudo").css("font-size", "20px");
-            }
+            }*/
 
             clearInterval(id);
             document.getElementById(form_id).submit();
         } else {
             pos = pos + 0.5;
 
-            // Si le joueur a cliqué sur la proposition de gauche, les points vont vers la gauche, sinon vers la droite
-            if(form_id == 'proposition1_form') {
-                $("#score").css("margin-right", pos + '%');
-            } else {
-                $("#score").css("margin-left", pos + '%');
-            }
+            // On déplace les scores de chaque côté
+            $("#score_right").css("margin-left", pos + '%');
         }
     }
 }
